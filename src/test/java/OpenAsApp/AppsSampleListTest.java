e445d36f9a8ff4174f6770fe601db3c5de2fd629
VerevkinAl <v.alex@ukr.net>
2017-03-05 11:26:19 +0200

    qsf

--- src/test/java/OpenAsApp/AppsSampleListTest.java
+++ src/test/java/OpenAsApp/AppsSampleListTest.java
@@ -1,18 +1,17 @@
 package OpenAsApp;
 
-
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class AppsSampleTest {
+public class AppsSampleListTest {
     @Test
-    public void AppsSample() {
+    public void AppsSampleList() {
         HomePage homePage = new HomePage();
         homePage.login("v--alex@mail.ru", "axaxa111");
         WebDriver driver = homePage.getDriver();
         AppsSample appsSample = new AppsSample(driver);
         appsSample.appSampleCreate();
-        appsSample.SalespeopleList();
+        appsSample.ListWithLocationData();
         appsSample.CreateExtension();
     }
 }
